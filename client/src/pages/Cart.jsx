aaf16957ec60fc95e586e0fa990128e258b5a30b
Spencer Cox <spencox@gmail.com>
2024-02-29 21:25:56 -0600

    got basic pages for signup, login, and home set up with react routing. Using chakra Ui for css, next will be to build out aditional site pages

---
+++ client/src/pages/Cart.jsx
@@ -0,0 +1,13 @@
+import { Box, Text } from "@chakra-ui/react"
+
+export default function Cart() {
+    return (
+    <Box height='100vh' weight='100vw' bg='blue' >
+        <Text>
+          Home
+        </Text>
+    </Box>
+    )        
+}
+
+
