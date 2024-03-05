bffedc6834ddf370f06382c38811d48b8043b9b4
MarkRBishop <mrb.8@hotmail.com>
2024-03-04 19:20:12 -0600

    added login mutation to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,6 +15,16 @@ const resolvers = {
                 const user = await User.create({ username, email, password });
                 const token = await signToken(user);
                 return { token, user };
+        },
+        loginUser: async (parent, { username, password }) => {
+            const user = await User.findOne({ username })
+
+            if (!user || !(await user.isCorrectPassword(password))) {
+                throw AuthenticationError;
+            }
+
+            const token = await signToken(user)
+            return { token, user }
         }
     }
 }
