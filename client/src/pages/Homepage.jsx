6af9c21e901bc53e8bd7f98505f9a546b2539061
Spencer Cox <spencox@gmail.com>
2024-03-07 19:41:16 -0600

    got basic cart functionality working. Can add items to cart and cart appears and adds total

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Container } from "@chakra-ui/react";
 import ItemList from '../components/ItemList';
+import Cart from "../components/Cart";
 
 export default function Homepage() {
   return (
     <Container maxW="100%">
       <ItemList />
+      <Cart />
     </Container>
   );      
 }
