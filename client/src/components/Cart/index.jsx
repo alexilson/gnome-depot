20af966125fdea2de10f04a018b8ad23347d518f
Spencer Cox <spencox@gmail.com>
2024-03-08 21:40:32 -0600

    updated cart icon to custom icon

--- client/src/components/Cart/index.jsx
+++ client/src/components/Cart/index.jsx
@@ -7,7 +7,7 @@ import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
-import icon from '/images/cart_icon.png'
+import icon from '/images/cart.png'
 import './style.css';
 
 // stripePromise returns a promise with the stripe object as soon as the Stripe package loads
