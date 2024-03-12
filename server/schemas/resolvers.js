dbc90c9e8613bcb7e9d4a71343afdb498c07c6cd
MarkRBishop <mrb.8@hotmail.com>
2024-03-11 21:15:24 -0500

    fixed resolver error

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,7 +2,6 @@ const { User, Item, Order } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
 require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET);
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
     Query: {
