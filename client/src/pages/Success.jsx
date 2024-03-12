1be0262799023628bcdac335f5ccf429c5633f20
alexilson <ilsonalex@gmail.com>
2024-03-11 19:29:19 -0700

    Added clear DB cart to Success page

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -1,7 +1,13 @@
 import { Flex } from "@chakra-ui/react";
+import { useMutation } from '@apollo/client';
+import { CLEAR_DB_CART } from '../utils/mutations';
 
 function Success() {
+
+  const [clearDbCart, { error }] = useMutation(CLEAR_DB_CART);
+
   setTimeout(() => {
+    clearDbCart();
     window.location.assign('/');
   }, 3000);
 
